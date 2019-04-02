59538c5f3e29b9fba1eb7f97cdd5022e6c09cac2
mdstevens <Dsn4uf13>
2019-04-01 20:35:31 -0400

    Add ability to convert from px to rem and rem to px

--- application/js/converter.js
+++ application/js/converter.js
@@ -2,18 +2,54 @@
   document.getElementById("afterConvert").value = (20 / 14) + "rem";
 }());
 
+function changeConversionType()
+{
+  const conversionType = document.getElementById("conversionType").value;
+
+  switch(conversionType)
+  {
+    case "pxToRem":
+      document.getElementById("beforeConvertLabel").innerText = "Pixels";
+      document.getElementById("afterConvertLabel").innerText = "Result in rem";
+      document.getElementById("beforeConvert").value = 20;
+      break;
+    case "remToPx":
+      document.getElementById("beforeConvertLabel").innerText = "Rem";
+      document.getElementById("afterConvertLabel").innerText = "Result in pixels";
+      document.getElementById("beforeConvert").value = 1;
+      break;
+  }
+
+  convert();
+}
+
 function convert()
 {
-  const beforeConvert = parseFloat(document.getElementById("beforeConvert").value.replace("px", ""));
+  const conversionType = document.getElementById("conversionType").value;
+  const beforeConvert = parseFloat(document.getElementById("beforeConvert").value);
   const defaultFontSize = parseFloat(document.getElementById("defaultFontSize").value);
 
-  if((!isNaN(beforeConvert)) && (!isNaN(defaultFontSize)))
+  switch(conversionType)
   {
-    document.getElementById("afterConvert").value = (beforeConvert / defaultFontSize) + "rem";
-  } else {
-    document.getElementById("afterConvert").value = "";
+    case "pxToRem":
+      if((!isNaN(beforeConvert)) && (!isNaN(defaultFontSize)))
+      {
+        document.getElementById("afterConvert").value = (parseFloat(document.getElementById("beforeConvert").value) / defaultFontSize) + "rem";
+      } else {
+        document.getElementById("afterConvert").value = "";
+      }
+      break;
+    case "remToPx":
+      if((!isNaN(beforeConvert)) && (!isNaN(defaultFontSize)))
+      {
+        document.getElementById("afterConvert").value = (parseFloat(document.getElementById("beforeConvert").value) * defaultFontSize) + "px";
+      } else {
+        document.getElementById("afterConvert").value = "";
+      }
+      break;
   }
 }
 
+document.getElementById("conversionType").addEventListener('change', function() { changeConversionType(); });
 document.getElementById("defaultFontSize").addEventListener('keyup', function() { convert(); });
 document.getElementById("beforeConvert").addEventListener('keyup', function() { convert(); });
